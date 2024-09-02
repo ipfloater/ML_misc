28dd48ff75000810dea7a6c05615f85d990d1c75
Chang Lin <lin@coffee>
2024-09-01 22:59:55 -0400

    comments on options

--- test_wrapper.py
+++ test_wrapper.py
@@ -1,3 +1,7 @@
 from sklearn_wrapper.utils import *
 
+# generate <N> random records,
+# <p> features, <p2> features with information
+# perform_cv=True: perform cross-validation
+# include_stacking=True: building ensemble stacking classifier
 run_classifier_test(N=10000, p=20, p2=5, perform_cv=True, include_stacking=True)
