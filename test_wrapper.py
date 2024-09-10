bdd5ffe711a51bec5875d7f0195687e0a405237c
C Lin <ipfloater@users.noreply.github.com>
2024-09-10 12:50:25 -0400

    added performance curves

--- test_wrapper.py
+++ test_wrapper.py
@@ -4,4 +4,11 @@ from sklearn_wrapper.utils import *
 # <p> features, <p2> features with information
 # perform_cv=True: perform cross-validation
 # include_stacking=True: building ensemble stacking classifier
-run_classifier_test(N=10000, p=20, p2=5, perform_cv=True, include_stacking=True, plot=True)
+
+XX, yy, clf, stats0, popCaps0, binCaps0 = run_classifier_test(N=5000, p=20, p2=5, perform_cv=True, include_stacking=True, plot=True)
+
+# optional: generate & display performance curves
+# res = plot_performance_curves(popCaps0, binCaps0)
+# print(res.popCapCurves)
+# print(res.badCapCurves)
+
